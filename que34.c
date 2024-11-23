c3d9fac3bdb935828a24b7da19e33e8af0993136
Aditya wadpalliwar <adityawadpalliwar520@gmail.com>
2024-11-23 12:22:54 +0530

    initial code

--- que34.c
+++ que34.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 int main(){
@@ -27,5 +27,29 @@ int main(){
 
 
  }
- ..
+ ..*/
+ #include<stdio.h>
+#include<string.h>
+int main(){
+char a[100]={'\0'};
+printf("input a string: ");
+fgets(a, sizeof(a), stdin);
+
+a[strcspn(a, "\n")] = '\0';
+
+int l= strlen(a);
+for(int i=0; i<l; i++)
+printf("%c\n", a[i]);
+
+for (int len = 2; len <= l; len++)
+        for (int i = 0; i <= l - len; i++) 
+        { 
+            for (int j = 0; j < len; j++) 
+                printf("%c", a[i + j]);
+            
+            printf("\n");
+        }
+    
+    return 0;
+}
     
