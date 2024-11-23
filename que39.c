20c3c4e58fa12593d6c1aaa3d9ee72a4590b8723
Aditya wadpalliwar <adityawadpalliwar520@gmail.com>
2024-11-23 10:09:53 +0530

    intial code

--- que39.c
+++ que39.c
@@ -1,1 +1,34 @@
-..
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+    int  i, j,k=0,m=0;
+ 
+    char a[50];
+	char l[50];
+   int fre[26]={0};
+
+    printf("enter the string: ");
+    fgets(a,50,stdin);
+    a[strlen(a)-1]='\0';
+    
+    
+    for (i = 0;a[i]!='\0'; i++)
+	{
+        fre[a[i]-'a']++;
+    }
+
+
+    for(i=0;i<26;i++)
+    {
+        if(fre[i]==0||fre[i]==i+1)
+        k++;
+    }
+
+
+    if(k==26)
+    printf("yes");
+    else
+    printf("no");
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,10 @@
 
-            break;
+#include<stdio.h>
+void main()
+{
+    char a='P';
+    char b='x';
+    char c=(a&b);
+    char e=(a^b);
+    printf("%c   %d",c,e);
+}
