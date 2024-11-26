ac994a1ff2df4e62c514a6499ca982bbea683eae
Aditya wadpalliwar <adityawadpalliwar520@gmail.com>
2024-11-26 21:32:28 +0530

    initial code

--- que12.c
+++ que12.c
@@ -9,25 +9,20 @@ int main(){
     printf("enter the string: ");
     fgets(string,50,stdin);
 
-  for(int i=0;string[i]!='\0';i++)
-  {  int m=0;
-    for(int j=0;string[j]!='\0';j++)
-    {
-        if (string[i]==string[j])
-        m++;
-    }
-    occr[i]=m;
-
-  }
- int maxindex=0;
- for (int i = 1; i < 50; i++) {
-        if (occr[i] > occr[maxindex]) {
+ int fr[26]={0};
+ for(int i=0;i<strlen(string);i++)
+ {
+    fr[string[i]-'a']++;
+ }
+int maxindex=0;
+int minindex=0;
+ for (int i = 0; i < strlen(string); i++) {
+        if (fr[i] > fr[maxindex]) {
             maxindex = i; 
         }
-    }
-int minindex=0;
- for (int i = 1; i < 50; i++) {
-        if (occr[i] < occr[maxindex]) {
+    
+ 
+        if (fr[i] < fr[minindex]) {
             minindex = i; 
         }
     }
